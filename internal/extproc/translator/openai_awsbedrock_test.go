3ea774e125712f0173695a275d78d9acef59845a
Aaron Choo <achoo30@bloomberg.net>
2025-02-28 13:04:26 -0500

    backport: e2e: add agent test using tool (#426) (#442)
    **Commit Message**
    
    * adding e2e test making sure that we can use a tool and return the
    proper response from the LLM
    
    This PR also contains a few fixes that allow this test to work 
    * added validateToolCallID as its needed for the bedrock message or it
    fails
    * Validate and cast the openai content value into bedrock content block
    - before we assumed it was always a string but it can also be an array
    * Merge the content blocks if one has text and the other has toolcall
    but no text - bedrockResp.Output.Message.Content is not 1:1 with openai
    choice.Message.Content.. we get the result in chunks that should be
    merged, for example the text and it's toolconfig were in seperate
    elements but openai expects them to be in the same
    * in openAIMessageToBedrockMessageRoleAssistant we assume that either
    there is a refusal or a content text, but we actually dont pass in a
    text. this was causing an error as the length of the array was set to 1
    so the first was empty and there must be a key specified in each content
    element.
    
    note: i think this is another bug that im trying to look into/create
    unit test for, but since there is already a lot in this PR, maybe its
    best to follow up with that one the next one. basically, the assistant
    openai param message's content doesn't seem to be translating to this
    method, and we have no content. this doesn't prevent anything else from
    working though, we are just missing a text field like
    `{"content":[{"text":"Certainly! I can help you get the weather
    information for New York City. To do that, I'll use the available
    weather tool. Let me fetch that information for you right away."}`
    
    the rests are tests
    
    ---------
    
    Signed-off-by: Alexa Griffith <agriffith96@gmail.com>
    Signed-off-by: Alexa Griffith  <agriffith96@gmail.com>
    Signed-off-by: Dan Sun <dsun20@bloomberg.net>
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>
    Co-authored-by: Alexa Griffith <agriffith96@gmail.com>
    Co-authored-by: Dan Sun <dsun20@bloomberg.net>
    Co-authored-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/extproc/translator/openai_awsbedrock_test.go
+++ internal/extproc/translator/openai_awsbedrock_test.go
@@ -79,6 +79,14 @@ func TestOpenAIToAWSBedrockTranslatorV1ChatCompletion_RequestBody(t *testing.T) 
 						}, Type: openai.ChatMessageRoleUser,
 					},
 					{
+						Value: openai.ChatCompletionToolMessageParam{
+							Content: openai.StringOrArray{
+								Value: "Weather in Queens, NY is 70F and clear skies.",
+							},
+							ToolCallID: "call_6g7a",
+						}, Type: openai.ChatMessageRoleTool,
+					},
+					{
 						Value: openai.ChatCompletionAssistantMessageParam{
 							Content: openai.ChatCompletionAssistantMessageParamContent{
 								Text: ptr.To("I dunno"),
@@ -129,6 +137,21 @@ func TestOpenAIToAWSBedrockTranslatorV1ChatCompletion_RequestBody(t *testing.T) 
 						Content: []*awsbedrock.ContentBlock{
 							{
 								Text: ptr.To("part2"),
+							},
+						},
+					},
+					{
+						Role: openai.ChatMessageRoleUser,
+						Content: []*awsbedrock.ContentBlock{
+							{
+								ToolResult: &awsbedrock.ToolResultBlock{
+									ToolUseID: ptr.To("call_6g7a"),
+									Content: []*awsbedrock.ToolResultContentBlock{
+										{
+											Text: ptr.To("Weather in Queens, NY is 70F and clear skies."),
+										},
+									},
+								},
 							},
 						},
 					},
@@ -907,7 +930,7 @@ func TestOpenAIToAWSBedrockTranslatorV1ChatCompletion_ResponseBody(t *testing.T)
 						Index: 0,
 						Message: openai.ChatCompletionResponseChoiceMessage{
 							Content: ptr.To("response"),
-							Role:    "assistant",
+							Role:    awsbedrock.ConversationRoleAssistant,
 						},
 						FinishReason: openai.ChatCompletionChoicesFinishReasonStop,
 					},
@@ -994,6 +1017,57 @@ func TestOpenAIToAWSBedrockTranslatorV1ChatCompletion_ResponseBody(t *testing.T)
 								},
 							},
 						},
+					},
+				},
+			},
+		},
+		{
+			name: "merge content",
+			input: awsbedrock.ConverseResponse{
+				Usage: &awsbedrock.TokenUsage{
+					InputTokens:  10,
+					OutputTokens: 20,
+					TotalTokens:  30,
+				},
+				Output: &awsbedrock.ConverseOutput{
+					Message: awsbedrock.Message{
+						Role: awsbedrock.ConversationRoleAssistant,
+						Content: []*awsbedrock.ContentBlock{
+							{Text: ptr.To("response")},
+							{ToolUse: &awsbedrock.ToolUseBlock{
+								Name:      "exec_python_code",
+								ToolUseID: "call_6g7a",
+								Input:     map[string]interface{}{"code_block": "from playwright.sync_api import sync_playwright\n"},
+							}},
+						},
+					},
+				},
+			},
+			output: openai.ChatCompletionResponse{
+				Object: "chat.completion",
+				Usage: openai.ChatCompletionResponseUsage{
+					TotalTokens:      30,
+					PromptTokens:     10,
+					CompletionTokens: 20,
+				},
+				Choices: []openai.ChatCompletionResponseChoice{
+					{
+						Index: 0,
+						Message: openai.ChatCompletionResponseChoiceMessage{
+							Content: ptr.To("response"),
+							Role:    awsbedrock.ConversationRoleAssistant,
+							ToolCalls: []openai.ChatCompletionMessageToolCallParam{
+								{
+									ID: "call_6g7a",
+									Function: openai.ChatCompletionMessageToolCallFunctionParam{
+										Name:      "exec_python_code",
+										Arguments: "{\"code_block\":\"from playwright.sync_api import sync_playwright\\n\"}",
+									},
+									Type: openai.ChatCompletionMessageToolCallTypeFunction,
+								},
+							},
+						},
+						FinishReason: openai.ChatCompletionChoicesFinishReasonStop,
 					},
 				},
 			},
@@ -1178,14 +1252,14 @@ func TestOpenAIToAWSBedrockTranslator_convertEvent(t *testing.T) {
 		{
 			name: "role",
 			in: awsbedrock.ConverseStreamEvent{
-				Role: ptrOf("assistant"),
+				Role: ptrOf(awsbedrock.ConversationRoleAssistant),
 			},
 			out: &openai.ChatCompletionResponseChunk{
 				Object: "chat.completion.chunk",
 				Choices: []openai.ChatCompletionResponseChunkChoice{
 					{
 						Delta: &openai.ChatCompletionResponseChunkChoiceDelta{
-							Role:    "assistant",
+							Role:    awsbedrock.ConversationRoleAssistant,
 							Content: &emptyString,
 						},
 					},
